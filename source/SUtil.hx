d396b68370f7f67afaf2ed2db3b6c14e8680f082
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2023-07-09 13:00:37 +0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -95,7 +95,7 @@ class SUtil
 		var stack:Array<String> = [];
 		stack.push(e.error);
 
-		for (stackItem in CallStack.exceptionStack(true))
+		for (stackItem in CallStack.exceptionStack)
 		{
 			switch (stackItem)
 			{
